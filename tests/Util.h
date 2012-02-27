6d44b52f35031a178df2c7343ce682129b2310c2
kbinani <kbinani@msn.com>
2012-02-27 22:22:52 +0900

    テスト結果をxmlに出力するオプションを追加

--- tests/Util.h
+++ tests/Util.h
@@ -5,6 +5,7 @@
 #include <cppunit/TestResult.h>
 #include <cppunit/TestResultCollector.h>
 #include <cppunit/CompilerOutputter.h>
+#include <cppunit/XmlOutputter.h>
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,20 +1,44 @@
 #include "Util.h"
+#include <string>
+
+using namespace std;
 
 int main( int argc, char* argv[] )
 {
+    string xmlPath = "";
+    string currentParse = "";
+    for( int i = 1; i < argc; i++ ){
+        string s = argv[i];
+        if( s.substr( 0, 1 ) == "-" ){
+            currentParse = s;
+        }else{
+            if( currentParse == "-o" ){
+                xmlPath = s;
+            }
+            currentParse = "";
+        }
+    }
     CppUnit::TestResult controller;
     CppUnit::TestResultCollector results;
     controller.addListener( &results );
 
     CppUnit::BriefTestProgressListener progress;
-    controller.addListener( &progress );
+    if( xmlPath.size() == 0 ){
+        controller.addListener( &progress );
+    }
 
     CppUnit::TestRunner runner;
     runner.addTest( CppUnit::TestFactoryRegistry::getRegistry().makeTest() );
     runner.run( controller );
 
-    CppUnit::CompilerOutputter outputter( &results, CppUnit::stdCOut() );
-    outputter.write();
+    if( xmlPath.size() == 0 ){
+        CppUnit::CompilerOutputter outputter( &results, CppUnit::stdCOut() );
+        outputter.write();
+    }else{
+        std::ofstream ofs( xmlPath.c_str() );
+        CppUnit::XmlOutputter outputter( &results, ofs, "UTF-8" );
+        outputter.write();
+    }
 
     return results.wasSuccessful() ? 0 : 1;
 }
