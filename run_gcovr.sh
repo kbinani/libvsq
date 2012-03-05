94763e38e8290d23f7716356fb5185078726e9cc
kbinani <kbinani@msn.com>
2012-03-05 13:53:19 +0900

    excluded test files from coverage report

--- run_gcovr.sh
+++ run_gcovr.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 directory=$(cd $(dirname $0);pwd)
-cd $directory && python ../../gcovr --root="$directory/" -exclude=$directory/tests --xml > coverage.xml.tmp
-cat $directory/coverage.xml.tmp | sed -e 's:filename=\"/:filename=\":g' > $directory/coverage.xml
-rm -f $direcgtory/coverage.xml.tmp
+cd $directory && python ../../gcovr --root="$directory/" --exclude="$directory/tests/*" --xml | sed -e 's:filename=\"/:filename=\":g' > $directory/coverage.xml
+
