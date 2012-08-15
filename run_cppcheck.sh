081f693c9956a915b31fea39989eb5b48ce6c275
kbinani <kbinani@msn.com>
2012-08-16 03:47:58 +0900

    remove unused shell and Makefile for CI

--- run_cppcheck.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-directory=$(cd $(dirname $0);pwd)
-cppcheck $directory --enable=all -q --xml 2>$directory/cppcheck-result.xml
-
--- run_gcovr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-directory=$(cd $(dirname $0);pwd)
-cd $directory && python ../../gcovr --root="$directory/" --exclude="$directory/tests/*" --xml | sed -e 's:filename=\"/:filename=\":g' > $directory/coverage.xml
-
